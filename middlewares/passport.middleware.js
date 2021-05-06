df202ad204430e494f8ef8fffbaf77a0064090dc
Trịnh Quang Nghĩa <tonystrinh@gmail.com>
2021-05-06 22:52:21 +0700

    Bring google id and secret to .env

--- middlewares/passport.middleware.js
+++ middlewares/passport.middleware.js
@@ -1,8 +1,6 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
 
-const usersModel = require("../models/users");
-
 passport.serializeUser((user, done) => {
   done(null, user);
 });
@@ -14,9 +12,8 @@ passport.deserializeUser((obj, done) => {
 passport.use(
   new GoogleStrategy(
     {
-      clientID:
-        "344651392176-o3jljdio8panmimubijosi5i7300j25u.apps.googleusercontent.com",
-      clientSecret: "Pb3L7pPsBdkrDWLOHOaQMEiB",
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL: `${process.env.HOST}/login/google/callback`,
     },
     function (token, refreshToken, profile, done) {
@@ -31,7 +28,6 @@ passport.use(
             "@foody.com",
             "@sea.com",
             "@garena.com",
-            // "@gmail.com"
           ].some((domain) => email.includes(domain))
         ) {
           return done(null, { email });
